991303b077851a39b95b5637bc9b246552573d77
Irina Spasova <irina.spasova.81@gmail.com>
2024-04-23 21:57:13 +0300

    buttons changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,9 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
-// import ServiceCard from "./ServiceCard";
+import ServiceCard from "./ServiceCard";
 // import ReviewCard from "./ReviewCard";
 
-// export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
+// export {  ReviewCard };
 
-export { Button, Nav, ShoeCard, PopularProductCard };
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard };
