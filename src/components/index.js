f0d80440273a2b04c6c320c57548544823cc2d6c
Irina Spasova <irina.spasova.81@gmail.com>
2024-04-23 21:35:55 +0300

    3 sections ready

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-// import Button from "./Button";
+import Button from "./Button";
 import Nav from "./Nav";
-// import ShoeCard from "./ShoeCard";
-// import PopularProductCard from "./PopularProductCard";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
 // import ServiceCard from "./ServiceCard";
 // import ReviewCard from "./ReviewCard";
 
 // export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
 
-export { Nav };
+export { Button, Nav, ShoeCard, PopularProductCard };
